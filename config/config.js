878419caa2d0aaa74b89b19884322a97f806786f
chelosilvero78 <marcesilvero@gmail.com>
2020-05-24 16:13:23 -0400

    mi primer commit

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '230470470708-apcrehk60o8kihjbhkvlgdr6cfk6j7lj.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'qGuMQmA6-1zV3_mBIFRH_khI';
